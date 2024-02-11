dab586d0afd0243a217b6ccb06efdeaffcc14efe
Guilherme Viana <imluizguilara@hotmail.com>
2024-02-10 21:06:47 -0300

    fix: fix build errors 2

---
+++ src/components/tabs/about/index.tsx
@@ -0,0 +1,48 @@
+import { motion } from 'framer-motion'
+import './style.css'
+
+export const About = () => {
+	return (
+		<motion.div
+			initial={{ opacity: 0, x: -15 }}
+			animate={{ opacity: 1, x: 0 }}
+			transition={{ duration: 0.4 }}
+		>
+			<div className='flex flex-col justify-between gap-20 p-2 text-base md:text-lg bg-[url("/assets/circlesBG.png")] bg-center bg-cover bg-no-repeat'>
+				<div className='flex flex-col gap-4'>
+					<h1 className='text-2xl font-bold flex gap-2'>
+						<span className='waving'>👋</span>
+						Olá, meu nome é Guilherme Viana!
+					</h1>
+
+					<p>
+						Sou um desenvolvedor <strong>front-end</strong> apaixonado por tecnologia e atuo no
+						<strong> desenvolvimento web</strong>.
+					</p>
+				</div>
+
+				<div className='flex flex-col gap-6'>
+					<h2 className='text-2xl font-bold'>Sobre mim</h2>
+					<p>
+						Possuo experiência em <strong>React</strong> & <strong>Next.js</strong>, além de
+						trabalhar com <strong>TypeScript</strong>.
+					</p>
+
+					<p>
+						Atualmente possuo cerca de 2 anos de estudos e cerca de 1 ano de experiência em projetos
+						reais focado na stack citada acima. Tento trazer soluções e apresentações{' '}
+						<strong>simples</strong> no meu dia a dia.
+					</p>
+
+					<p>Sou mente aberta quanto ao aprendizado e busco aprender um pouco mais a cada dia.</p>
+
+					<p>
+						Atualmente estou estudando <strong>Node.js</strong> e no futuro pretendo experimentar
+						outras áreas da programação, como ampliar meus conhecimentos nos ecossistemas que já
+						conheço e também atuar em outros escopos além do desenvolvimento web.
+					</p>
+				</div>
+			</div>
+		</motion.div>
+	)
+}
---
+++ src/components/tabs/projects/project-item.tsx
@@ -0,0 +1,156 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import * as Tooltip from '@radix-ui/react-tooltip'
+import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
+
+import { Project } from '@/@types/project'
+import { AiFillGithub, AiOutlineRightCircle } from 'react-icons/ai'
+import { BsFillPinAngleFill } from 'react-icons/bs'
+import { IoMdClose } from 'react-icons/io'
+
+export const ProjectItem = ({
+	deploy,
+	description,
+	imagePath,
+	repository,
+	technologies,
+	title,
+}: Project) => {
+	return (
+		<div className='relative flex flex-col gap-3 p-2 mx-2 border rounded-lg md:p-3 border-zinc-600 md:flex-row'>
+			<div className='absolute top-0 left-0 mt-[-10px] ml-[-15px]'>
+				<div className='flex items-center justify-center w-10 h-10 text-xl font-bold rounded-full bg-sky-600'>
+					<BsFillPinAngleFill />
+				</div>
+			</div>
+
+			<Dialog.Root>
+				<Dialog.Trigger asChild>
+					<img
+						src={imagePath}
+						alt=''
+						className='object-contain w-full max-h-72 md:w-1/2 rounded-xl hover:cursor-zoom-in'
+					/>
+				</Dialog.Trigger>
+				<Dialog.Portal>
+					<Dialog.Overlay className='fixed inset-0 bg-black/60' />
+					<motion.div
+						initial={{ opacity: 0, y: 0 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.25 }}
+					>
+						<Dialog.Content className='data-[state=open]:animate-contentShow w-full flex p-8 items-center justify-center fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] rounded-[6px]  shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none'>
+							<div className='flex flex-col gap-8 p-4 max text-white rounded-lg bg-neutral-900'>
+								<div className='flex justify-between'>
+									<Dialog.Title className='m-0 flex-1 text-xl tracking-tight text-center text-white font-bold md:text-3xl'>
+										~ {title}
+									</Dialog.Title>
+
+									<Dialog.Close asChild>
+										<button
+											className='text-violet11 hover:bg-violet4 focus:shadow-violet7 inline-flex h-10 w-10 appearance-none items-center justify-center rounded-full focus:outline-none'
+											aria-label='Close'
+										>
+											<IoMdClose className='w-6 h-6' />
+										</button>
+									</Dialog.Close>
+								</div>
+
+								<img
+									src={imagePath}
+									alt=''
+									className='object-contain w-full max overflow-hidden rounded-xl'
+								/>
+
+								<Dialog.Description className='px-2 md:px-10 text-sm leading-normal text-center text-gray-400 md:text-base'>
+									<p className='border-b border-zinc-600 inline-flex pb-1'>{description}</p>
+								</Dialog.Description>
+							</div>
+						</Dialog.Content>
+					</motion.div>
+				</Dialog.Portal>
+			</Dialog.Root>
+
+			<div className='flex flex-col flex-1 gap-3 p-3'>
+				<div className='flex flex-col gap-2 flex-wrap'>
+					<h2 className='text-xl font-bold truncate'>{title}</h2>
+					<p className='text-base text-gray-400 font-secondary'>{description}</p>
+				</div>
+				<div className='mt-3 text-base'>
+					<h4 className='text-lg font-bold'>Tecnologias utilizadas</h4>
+					<ul className='flex flex-wrap gap-2 pt-2'>
+						{technologies.map((item, index) => (
+							<Tooltip.Provider
+								delayDuration={0}
+								key={index}
+							>
+								<Tooltip.Root>
+									<Tooltip.Trigger asChild>
+										<li className='flex items-center justify-center w-8 h-8'>
+											<i key={index}>
+												<img
+													src={item.iconPath}
+													alt={item.name}
+													className='w-full'
+												/>
+											</i>
+										</li>
+									</Tooltip.Trigger>
+									<Tooltip.Portal>
+										<Tooltip.Content className='px-3 py-1 font-medium text-gray-800 bg-white rounded select-none'>
+											{item.name}
+											<Tooltip.Arrow className='mb-2 fill-white' />
+										</Tooltip.Content>
+									</Tooltip.Portal>
+								</Tooltip.Root>
+							</Tooltip.Provider>
+						))}
+					</ul>
+				</div>
+
+				<span className='flex gap-3 pt-5 mt-auto flex-wrap'>
+					{/* If deploy or repository isn't available its link is an empty string */}
+					{repository.trim() ? (
+						<Link
+							to={repository}
+							target='_blank'
+							className='inline-flex items-center gap-1 text-base transition duration-100 text-sky-600 hover:text-sky-700'
+						>
+							<i>
+								<AiFillGithub size={20} />
+							</i>
+							Repositório
+						</Link>
+					) : (
+						<span className='inline-flex items-center gap-1 text-base text-gray-500 cursor-not-allowed select-none'>
+							<i>
+								<AiFillGithub size={20} />
+							</i>
+							Repositório
+						</span>
+					)}
+
+					{deploy.trim() ? (
+						<Link
+							to={deploy}
+							target='_blank'
+							className='inline-flex items-center gap-1 text-base transition duration-100 text-sky-600 hover:text-sky-700'
+						>
+							<i>
+								<AiOutlineRightCircle size={20} />
+							</i>
+							Deploy
+						</Link>
+					) : (
+						<span className='inline-flex items-center gap-1 text-base text-gray-500 cursor-not-allowed select-none'>
+							<i>
+								<AiOutlineRightCircle size={20} />
+							</i>
+							Deploy
+						</span>
+					)}
+				</span>
+			</div>
+		</div>
+	)
+}
