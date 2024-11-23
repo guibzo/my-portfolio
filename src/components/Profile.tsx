c694cc0992132c0c19099ca546fc445b77e90d3b
Guilherme Viana <imluizguilara@hotmail.com>
2024-11-23 09:04:18 -0300

    change pfp

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,5 @@
 import * as Tooltip from '@radix-ui/react-tooltip'
-import ProfilePicture from '/assets/profileImage4.png'
+import ProfilePicture from '/assets/profile-picture.png'
 
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card'
 import { contacts } from '@/constants/contacts'
